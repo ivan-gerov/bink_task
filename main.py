e22ffe1aea6e56203726a71a86f5dfcc5485232c
Ivan Gerov <igerov11@gmail.com>
2021-09-25 18:31:38 +0300

    Added task 3

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-
-
 import argparse
 
 from common.utils import sort_records, parse_csv_to_dicts
+from collections import Counter
 
 
 def task_1(records):
@@ -24,29 +23,40 @@ def task_2(records):
     print("\nTotal rent of all items in the list:\n")
     print(sum([item["Current Rent"] for item in lease_25_years]))
 
+
+def task_3(records):
+    """Print the count of all masts grouped by tenant"""
+
+    mast_counts = dict()
+    for record in records:
+        tenant = record["Tenant Name"]
+        mast_counts[tenant] = mast_counts.get(tenant, 0) + 1
+    
+    for tenant, no_of_masts in mast_counts.items():
+        print(f"Tenant: {tenant}, Number of masts: {no_of_masts}")
+
+
 def task_controller(task_id, data):
-    """ Runs any of the four tasks """
-    tasks = {"1": task_1,
-             "2": task_2,}
-            #  "3": task_3,
-            #  "4": task_4}
+    """Runs any of the four tasks"""
+    tasks = {
+        "1": task_1,
+        "2": task_2,
+        "3": task_3,
+    }
+    #  "4": task_4}
     if task_id not in tasks.keys():
         raise ValueError("Please provide a valid task id")
 
     records = parse_csv_to_dicts(data)
     tasks[task_id](records)
-    
+
 
 if __name__ == "__main__":
     DATA = "data/Python Developer Test Dataset.csv"
 
     ap = argparse.ArgumentParser()
-    ap.add_argument("-t", "--task", required=True,
-                    help="Task id")
+    ap.add_argument("-t", "--task", required=True, help="Task id")
     ap.description = "Hi! You are required to provide an integer between 1-4 to run any of the four tasks! Thanks! :)"
     args = vars(ap.parse_args())
 
     task_controller(args["task"], data=DATA)
-
-
-    
